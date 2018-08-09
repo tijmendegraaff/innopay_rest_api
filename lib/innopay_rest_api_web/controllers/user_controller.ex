28ecc3aa56223ad01e69e595fd36b90bd0143dc1
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-09 22:03:08 +0200

    add signin function to user controller

--- lib/innopay_rest_api_web/controllers/user_controller.ex
+++ lib/innopay_rest_api_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule InnopayRestApiWeb.UserController do
   use InnopayRestApiWeb, :controller
 
+  alias InnopayRestApi.Guardian
   alias InnopayRestApi.Accounts
   alias InnopayRestApi.Accounts.User
 
@@ -12,11 +13,18 @@ defmodule InnopayRestApiWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", user_path(conn, :show, user))
-      |> render("show.json", user: user)
+    with {:ok, %User{} = user} <- Accounts.create_user(user_params), 
+      {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
+      conn |> render("jwt.json", jwt: token)
+    end
+  end
+
+  def sign_in(conn, %{"email" => email, "pin" => pin}) do
+    case Accounts.token_sign_in(email, pin) do
+      {:ok, token, _claims} ->
+        conn |> render("jwt.json", jwt: token)
+      _ ->
+        {:error, :unauthorized}
     end
   end
 
