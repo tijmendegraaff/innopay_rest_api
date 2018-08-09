be09c3b869900e52ab6b12baadf21c781c45a23d
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-09 21:11:25 +0200

    setup config for guardian

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
+# Guardian config
+config :myApi, InnopayRestApi.Guardian,
+  issuer: "InnopayRestApi",
+  secret_key: "xs1QqqUvhrDdSfqUc6wVuzimFaJq0DBPkp/Ri/qFOU0/A7lavUv8Xp/48STTTJFv"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
