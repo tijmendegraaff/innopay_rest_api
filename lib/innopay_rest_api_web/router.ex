5eefaaa2727aeb8f51cb4e97e46d0728eb8028ce
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-09 22:03:17 +0200

    add signin route

--- lib/innopay_rest_api_web/router.ex
+++ lib/innopay_rest_api_web/router.ex
@@ -8,6 +8,7 @@ defmodule InnopayRestApiWeb.Router do
   scope "/api/v1", InnopayRestApiWeb do
     pipe_through :api
 
-    resources "/users", UserController, only: [:create, :show]
+    post "/sign_up", UserController, :create
+    post "/sign_in", UserController, :sign_in
   end
 end
